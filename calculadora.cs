1b1e61701155836f0e8e26484f4cbf5de806ea21
Zero Seventty <47706299+Seventty@users.noreply.github.com>
2019-09-16 01:43:06 -0300

    Update calculadora.cs

--- calculadora.cs
+++ calculadora.cs
@@ -2,78 +2,87 @@ using System;
 namespace algo{
 	class calculadora{
 		public static void Main(){
-		Console.WriteLine("Bienvenido a una calculadora en c#\nPresiona cualquier tecla para continuar...");
-		Console.ReadKey();
-		Console.Clear();
-		Menu();
-	}
+						Console.WriteLine("Bienvenido a una calculadora en c#\nPresiona cualquier tecla para continuar...");
+						Console.ReadKey();
+						Console.Clear();
+						Menu();
+					}
+/*############################### Menu aquí arriba #################################*/
 		public static void Menu(){
-			Console.WriteLine("Introduce el operador a usar: Suma, Resta, Multiplicacion, Division.");
+			double operando1 = 0;
+			double operando2 = 0;
+			double resultado = 0;
+			string resultadofinal = "";
+			Console.WriteLine("Introduce el operador a usar: Suma, Resta, Multiplicacion, Division.   (Tambien puede usar Salir e Historial)");
 			string lector = Console.ReadLine().ToLower();
 			switch(lector){
 				case "suma":
-				suma();
-				break;
+				Console.Clear();
+				Console.WriteLine("Introduce un operando: ");
+				operando1 = Convert.ToDouble(Console.ReadLine());
+				Console.WriteLine("Introduce el segundo operando: ");
+				operando2 = Convert.ToDouble(Console.ReadLine());
+				resultado = suma(operando1,operando2);
+				resultadofinal = "El resultado de la operación {"+lector+"}: "+operando1.ToString()+" + "+operando2+" = "+resultado;
+				Console.WriteLine(resultadofinal);
+				Console.ReadKey();
+				Console.WriteLine("\n¿Quiere realizar otra operacion? Si o No");
+				string confirmacion = Console.ReadLine().ToLower();
+				if(confirmacion == "si"){
+					Console.Clear();
+					Console.WriteLine("Regresando al menu...");
+					Menu();
+				}else{
+					Console.Clear();
+					Console.WriteLine("Vuelva pronto.");
+					Console.ReadKey();
+				}
+
+					break;
 
 				case "resta":
-				resta();
-				break;
+				//resta();
+				Console.WriteLine("En desarrollo");
+				Console.ReadKey();
+					break;
 
 				case "multiplicacion":
-				multiplicacion();
+				//multiplicacion();
+				Console.WriteLine("En desarrollo");
+				Console.ReadKey();
 				break;
 
 				case "division":
-				division();
-				break;
+				//division();
+				Console.WriteLine("En desarrollo");
+				Console.ReadKey();
+					break;
+
+				case "historial":
+				Console.WriteLine("En desarrollo...");
+				Console.ReadKey();	
+
+				case "salir":
+				Console.WriteLine("Vuelva pronto...");
+				Console.ReadKey();
+					break;
 
 				default:
 				Console.WriteLine("\nIntroduce una opción valida.");
 				Console.ReadKey();
 				Console.Clear();
 				Menu();
-				break;
-			}
-		}public static void suma(){
-			Console.Clear();
-			Console.WriteLine("Introduce los operandos a sumar: ");
-			int respuesta = Convert.ToInt32(Console.ReadLine());
-				if(respuesta <= 1){
-					Console.WriteLine("Introduce mas de un operando para realizar la operacion.");
-					Console.ReadKey();
-					Console.Clear();
-					suma();
-				}
-			}public static void resta(){
-				Console.Clear();
-			Console.WriteLine("Introduce los operandos a restar: ");
-			int respuesta = Convert.ToInt32(Console.ReadLine());
-				if(respuesta <= 1){
-					Console.WriteLine("Introduce mas de un operando para realizar la operacion.");
-					Console.ReadKey();
-					Console.Clear();
-					resta();
-				}
-				}public static void multiplicacion(){
-					Console.Clear();
-			Console.WriteLine("Introduce los operandos a multiplicar: ");
-			int respuesta = Convert.ToInt32(Console.ReadLine());
-				if(respuesta <= 1){
-					Console.WriteLine("Introduce mas de un operando para realizar la operacion.");
-					Console.ReadKey();
-					Console.Clear();
-					multiplicacion();
-				}
-					}public static void division(){
-						Console.Clear();
-			Console.WriteLine("Introduce los operandos a dividir: ");
-			int respuesta = Convert.ToInt32(Console.ReadLine());
-				if(respuesta <= 1){
-					Console.WriteLine("Introduce mas de un operando para realizar la operacion.");
-					Console.ReadKey();
-					Console.Clear();
-					division();
-				}
+					break;
+			}/*################### Fin del menú ############################*/
+/*################################### Operadores matematicos ##############################*/
+		}public static double suma(double operador1, double operador2){
+			return operador1 + operador2;
+		}public static double resta(double operador1, double operador2){
+			return operador1 - operador2;
+		}public static double multiplicacion(double operador1, double operador2){
+			return operador1 * operador2;
+		}public static double division(double operador1, double operador2){
+			return operador1 / operador2;
 		}
 	}
 }
